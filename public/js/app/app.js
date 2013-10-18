ca30228b95f968dc71fb8441eac18b2dab67fed9
Pete Dunlap <petedunlap@gmail.com>
2013-10-18 12:01:11 -0500

    build first feature

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,15 +1,48 @@
 'use strict';
+var concert = {};
 
 $(document).ready(initialize);
 
 function initialize(){
   $(document).foundation();
-
+  $('#createSeats').click(clickCreateSeats);
 }
 
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
+
+funcion clickCreateSeats(){
+  var seatNum = getValue('#seatNum',parseInt);
+  var section = $('#section').val();
+  putSeats(section, seatNum);
+}
+
+function putSeats(section, seatNum){
+  if(section=='vip'){
+    //vip
+    concert.vipCost = getValue('#seatNum',parseFloat);
+    htmlAddSeats('#vip');
+
+  }else{
+    //ga
+    concert.gaCost = getValue('#seatNum',parseFloat);
+    htmlAddSeats('#ga');
+  }
+
+}
+
+function htmlAddSeats(selector){
+  for(var i = 0; i<seatNum; i++){
+      $seat = $('<div>');
+      $seat.text('Seat # ' + (i+1));
+      $(selector).append($seat);
+    }
+}
+
+
+
+
 
 function getValue(selector, fn){
   var value = $(selector).val();
--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -13,18 +13,18 @@ function teardownTest(){ //code to run after test
 test('Sections Building', function(){
   expect(2);
 
-  $('#section option[value="vip"]').trigger('click');
+  $('#section').val('vip');
   $('#seatNum').val(45);
   $('#seatCost').val(20);
   $('#createSeats').trigger('click');
 
   deepEqual($('#vip div').length,45,"checking that 45 divs were put inside vip section");
 
-  $('#section option[value="ga"]').trigger('click');
+  $('#section').val('ga');
   $('#seatNum').val(80);
   $('#seatCost').val(10);
   $('#createSeats').trigger('click');
 
-  deepEqual($('#general div').length,45,"checking that 80 divs were put inside general admission section");
+  deepEqual($('#general div').length,80,"checking that 80 divs were put inside general admission section");
 
 });
